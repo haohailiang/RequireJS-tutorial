138a4b28b24e59dcdc2fdf56526b72a7ff28cf20
郝海亮 <2298763729@qq.com>
2018-04-21 11:27:08 +0800

    JS不在同一个路径下如意出错

--- js/helper.js
+++ js/helper.js
@@ -1,4 +1,8 @@
-define('helper', ['jquery'], function($) {
+define('helper', ['./lib/jquery'], function($) {
+	console.log($); //加载不到
+	require(['jquery'], function($) {
+		console.log($); //可以加载到
+	});
 	return {
 		trim: function(str) {
 			return $.trim(str);
