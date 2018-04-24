16fa070b55ecc8e51a60f570d501bcdf624c0576
郝海亮 <2298763729@qq.com>
2018-04-24 16:31:38 +0800

    添加helper

--- www/js/app.js
+++ www/js/app.js
@@ -1,16 +1,21 @@
 requirejs(['./require.config'], function () {
-	require(['app/api'], function(api) {
+	require(['app/api','i18n!nls/messages', 'app/helper'], function(api, i18n, helper) {
 
 		// promise使用
-		api.getUser().then(function(user) {
-			console.log(user);
-		});
+		// api.getUser().then(function(user) {
+		// 	console.log(user);
+		// });
 
 		// 传统JSONP使用1
-		api.getUserByJsonp();
+		// api.getUserByJsonp();
 
 		// requiresjs-jsonp使用
-		api.getUserByJsonp2();
+		// api.getUserByJsonp2();
+		
+		$('#jUser').after('<button class="btn btn-default">' + i18n.edit + '</button>');
+
+		var str = helper.trim('   amd   ');
+		console.log(str);
 	});
 });
 
--- www/js/require.config.js
+++ www/js/require.config.js
@@ -1,7 +1,25 @@
+var language = document.cookie.match(/language=([^;]+)/);
+var locale = 'zh';
+if(language) {
+	// document.cookie = "language=en_US"
+	locale = language[1].split('_')[0];
+}
+
 requirejs.config({
 	baseUrl: 'js/lib',
 	paths: {
-		app    : '../app'
+		app    : '../app',
+		nls    : '../nls'
+	},
+	config : {
+		text: {
+			onXhr: function(xhr, url) {
+				xhr.setRequestHeader('X-Requested-with', 'XMLHttpRequest')
+			}
+		},
+		i18n : {
+			locale : locale
+		}
 	}
 });
 
